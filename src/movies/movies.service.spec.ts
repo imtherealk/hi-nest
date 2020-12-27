0a03bfc10be951a2271909cb3dfec07a640a3e08
Nayeong <imtherealk@gmail.com>
2020-12-27 15:42:29 +0900

    Test update

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -36,7 +36,7 @@ describe('MoviesService', () => {
       expect(movie.id).toEqual(1);
       expect(movie.title).toEqual('Test Movie');
     });
-    it('should throw 404 error', () => {
+    it('should throw NotFoundException', () => {
       try {
         service.getOne(99999);
       } catch (e) {
@@ -58,7 +58,7 @@ describe('MoviesService', () => {
       expect(afterDelete).toBeLessThan(beforeDelete);
     });
 
-    it('should throw a 404 error', () => {
+    it('should throw a NotFoundException', () => {
       try {
         service.deleteOne(99999);
       } catch (e) {
@@ -77,6 +77,27 @@ describe('MoviesService', () => {
       });
       const afterCreate = service.getAll().length;
       expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2010,
+      });
+      service.update(1, { title: 'Updated Test' });
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('Updated Test');
+    });
+
+    it('should throw a NotFoundException', () => {
+      try {
+        service.update(99999, {});
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
     });
   });
 });
